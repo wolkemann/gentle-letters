1a1c4047b62475e6fc3ad4410eecc23c247eb927
Federico Sardo <federico.sardo@vistaglobal.com>
2024-08-27 11:53:02 +0200

    ui_improv - added footer content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full p-5 bg-stone-50 shadow">
-      <div className="md:container grid grid-rows-1 md:grid-cols-4 gap-10">
+    <footer className="w-full p-5 text-sm bg-stone-50 shadow">
+      <div className="md:container grid grid-rows-1 md:grid-cols-3 gap-40">
         <div className="flex flex-col justify-between">
-          <span>
-            <h3 className="font-bold">About Gentle Letters</h3>
-            <p>A open source project by Federico "Wolkemann" Sardo.</p>
-          </span>
-
+          <p>
+            A open source project by Federico "Wolkemann" Sardo.
+            <br />
+            If you like this project, please consider to donate.
+          </p>
           <Link
             href="https://github.com/wolkemann/gentle-letters"
             target="_BLANK"
@@ -18,23 +18,48 @@ export default function Footer() {
             Github Repository
           </Link>
         </div>
-        <div>
-          <span>
-            <h3 className="font-bold">Contacts</h3>
-            <p>A open source project by Federico "Wolkemann" Sardo.</p>
-          </span>
+        <div className="flex flex-col items-start">
+          <h3 className="font-bold mb-3">Contacts</h3>
+          <Link
+            href="https://portfolio-seven-roan-24.vercel.app/"
+            target="_BLANK"
+            className="my-2 hover:underline"
+          >
+            Portfolio
+          </Link>
+          <Link
+            href="mailto:federico.sardo1@AT_gmail.com"
+            target="_BLANK"
+            className="my-2 hover:underline"
+          >
+            Email
+          </Link>
+          <Link
+            href="https://github.com/wolkemann/gentle-letters"
+            target="_BLANK"
+            className="my-2 hover:underline"
+          >
+            Instagram
+          </Link>
+          <Link
+            href="https://github.com/wolkemann"
+            target="_BLANK"
+            className="my-2 hover:underline"
+          >
+            Github
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/federico-sardo-8187a3196/"
+            target="_BLANK"
+            className="mt-2 hover:underline"
+          >
+            Linkedin
+          </Link>
         </div>
         <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-          voluptatem enim delectus illo quibusdam minima molestiae nulla quas
-          unde, aut, eum accusamus accusantium, atque non ducimus cumque! Modi,
-          voluptatibus a.
-        </div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-          voluptatem enim delectus illo quibusdam minima molestiae nulla quas
-          unde, aut, eum accusamus accusantium, atque non ducimus cumque! Modi,
-          voluptatibus a.
+          <h3 className="font-bold mb-2">Cookies Policy</h3>
+          This site uses only necessary cookies, which are required to enable
+          its basic features, such as providing secure log-in.
         </div>
       </div>
     </footer>
