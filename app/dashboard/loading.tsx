27c23b9f938725220565154a03da51bf4d7c45b0
Federico Sardo <federico.sardo@vistaglobal.com>
2024-08-27 16:07:54 +0200

    ui_improv - quick fix

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,10 +1,13 @@
+import HeartLoader from "@/components/ui/heart-loader";
 import Window from "@/components/ui/window";
 
 export default function DashboardLoading() {
   return (
     <main>
       <section className="w-full min-h-screen flex flex-col items-center justify-center">
-        <Window title="Loading">Loading</Window>
+        <Window title="Loading">
+          <HeartLoader />
+        </Window>
       </section>
     </main>
   );
