2d0c81f3f6d234a4f4013d79a617ef137bc0dae0
Federico Sardo <federico.sardo@vistaglobal.com>
2024-08-28 11:42:47 +0200

    feature/admin_panel - admin letter block components

--- components/adminDashboard/AdminShuffleLetters.tsx
+++ components/adminDashboard/AdminShuffleLetters.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { isReplyTimePassed } from "@/utils/dateFormatters";
+import { getDateAsText, isReplyTimePassed } from "@/utils/dateFormatters";
 import Window from "../ui/window";
 import { Tables } from "@/types/supabase";
 import { useMemo } from "react";
+import { Mail, Shuffle } from "lucide-react";
+import { SubmitButton } from "../ui/submit-button";
 
 type AdminShuffleLettersProps = {
   letters: Tables<"letters">[] | undefined | null;
@@ -11,14 +13,35 @@ type AdminShuffleLettersProps = {
 export default function AdminShuffleLetters({
   letters,
 }: AdminShuffleLettersProps) {
-  const expiredLetters = useMemo(
-    () => letters?.filter((letter) => isReplyTimePassed(letter.created_at)),
+  // invalid letters are letters that have no recipientId or time for reply is expired without a replyId
+  const invalidLetters = useMemo(
+    () =>
+      letters?.filter(
+        ({ replyId, created_at, recipientId }) =>
+          (isReplyTimePassed(created_at) && replyId === null) ||
+          recipientId === null,
+      ),
     [letters],
   );
 
-  console.log(expiredLetters);
-
   return (
-    <Window title="Shuffle Letters" className="w-full md:w-[700px]"></Window>
+    <Window title="Shuffle Letters" className="w-full md:w-[700px]">
+      <div>
+        {invalidLetters?.map((letter) => (
+          <div
+            key={letter.id}
+            className="p-3 flex justify-between items-center text-sm bg-pink-300 rounded shadow"
+          >
+            <div>
+              {getDateAsText(letter.created_at)}{" "}
+              <span className="ml-3">Letter status</span>
+            </div>
+            <SubmitButton size="icon" className="rounded-full">
+              <Shuffle className="w-[20px]" />
+            </SubmitButton>
+          </div>
+        ))}
+      </div>
+    </Window>
   );
 }
